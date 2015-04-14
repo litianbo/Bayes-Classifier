5400bdeef1131ad91f188f6299d8cd7334304e5e
Tianbo <tianboli@usc.edu>
2015-04-14 00:32:25 -0700

    done for now

--- bayes.py
+++ bayes.py
@@ -23,6 +23,7 @@ def calculatePDF(x):
    #ml.append(pDF)
    ml.append(pFD)
    ml.append(pFnotD)
+   ml.append(findings)
    return ml
 def calculateP(x,y,z):
    p1=z
@@ -58,16 +59,67 @@ def calculateUP(x,y,z):
       minmax.append('{0:.4f}'.format(min(temp)))
       minmax.append('{0:.4f}'.format(max(temp)))
    return minmax
-def calculateq1(x,y):
-   outputFile=open('output.txt','w')
+def calculateq3(x,y,z,findings):
+   #print y
+   q3=list()
+   q1=calculateP(x,len(x)/2,z)
+   if len(y)==0:
+      temp=list()
+      temp.append("none")
+      temp.append("N")
+      temp.append("none")
+      temp.append("N")
+      q3.append(temp)
+   else:
+      temp=list()
+      for i in range(2*len(y)):
+         index=i/2
+         if i%2==0:
+            x.append(y[index][0])
+            x.append(y[index][1])
+         else:
+            x.append(y[index][2])
+            x.append(y[index][3])
+         temp.append(calculateP(x,len(x)/2,z))
+         del x[-1]
+         del x[-1]
+      temp2=list()
+      for i in range(len(temp)):
+         temp3=list()
+         temp3.append(temp[i])
+         temp3.append(findings[i/2])
+         if i%2==0:
+            temp3.append('T')
+         else:
+            temp3.append('F')
+         temp2.append(temp3)
+      if max(temp2)[0]<=q1:
+         q3.append('none')
+         q3.append('N')
+      else:
+         q3.append(max(temp2)[1])
+         q3.append(max(temp2)[2])
+      #print min(temp2)
+      if min(temp2)[0]>=q1:
+         q3.append('none')
+         q3.append('N')
+      else:
+         q3.append(min(temp2)[1])
+         q3.append(min(temp2)[2])
+   return q3
+def calculateq1(x,y,z):
+   fileName=z[:-4]+'_inference.txt'
+   outputFile=open(fileName,'w')
    probOfDisease=dict()
    minmaxDisease=dict()
+   nextFinding=dict()
    for i in range(len(y)):
       for j in range(len(y[i])):
          counter=0
          uCounter=0
          findings=list()
          uFindings=list()
+         uFindingNames=list()
          for k in range(len(y[i][j])):
             if y[i][j][k]!='U':
                counter+=1
@@ -84,17 +136,23 @@ def calculateq1(x,y):
                temp.append(1-x[j][2][k])
                temp.append(1-x[j][3][k])
                uFindings.append(temp)
+               uFindingNames.append(x[j][4][k])
+         #print uFindingNames
          p=calculateP(findings,counter,x[j][1])
          p2=calculateUP(findings,uFindings,x[j][1])
-         probOfDisease[(x[j][0])]='{0:.4f}'.format(p)
-         minmaxDisease[(x[j][0])]=p2
+         p3=calculateq3(findings,uFindings,x[j][1],uFindingNames)
+         probOfDisease[x[j][0]]='{0:.4f}'.format(p)
+         minmaxDisease[x[j][0]]=p2
+         nextFinding[x[j][0]]=p3
          #print(p2)
       outputFile.write('Patient-' + str(i+1)+':\n')
       outputFile.write(str(probOfDisease)+'\n')
       outputFile.write(str(minmaxDisease)+'\n')
+      outputFile.write(str(nextFinding)+'\n')
    outputFile.close()
 """ READ THE INPUT FILE """
 inputFile = open(sys.argv[2])
+fileName=str(sys.argv[2])
 a1=dict();
 firstline = inputFile.readline()
 numOfDiseases= int(firstline.split(' ')[0])
@@ -138,5 +196,5 @@ for i in range(numOfDiseases):
    #print(ml2)
    disease.append(calculatePDF(ml2))
    ml2=list()
-calculateq1(disease,patientsFindings)
+calculateq1(disease,patientsFindings,fileName)
       
